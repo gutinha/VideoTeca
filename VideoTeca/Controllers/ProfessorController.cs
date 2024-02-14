e34707044ee288a51ead909fd3a8d261f7f72212
Gustavo Mendes <73292746+gutinha@users.noreply.github.com>
2024-02-14 01:07:15 -0300

    Create view professor

---
+++ VideoTeca/Controllers/ProfessorController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VideoTeca.Models;
+
+namespace VideoTeca.Controllers
+{
+    public class ProfessorController : Controller
+    {
+        public dbContext db = new dbContext();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
